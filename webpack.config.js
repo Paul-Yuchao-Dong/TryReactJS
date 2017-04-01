5a64a04d278822b4e4de47688b37f0125666cd0b
Paul-Yuchao-Dong <paulcynic@gmail.com>
2017-04-01 15:24:07 +0800

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var debug = process.env.NODE_ENV ！== "production";
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    devtool: debug ? "inline-sourcemap": null,
+    entry: "./js/script.js",
+    output: {
+        path: __dirname + "/js",
+        filename: "script.min.js"
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: false,
+            sourceMap: false
+        }),
+    ],
+};
